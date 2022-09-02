053e4e4c355f87ccdf07f90bc46e48a4c459a641
Ching Yee <cauu0001@student.monash.edu>
2022-09-02 17:02:56 +1000

    week 4 -change default date of datapicker

---
+++ Week6/FIT5032_MyViewModel/FIT5032_MyViewModel/Models/SampleFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FIT5032_MyViewModels.Models
+{
+    public class SampleFormViewModels
+    { }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
