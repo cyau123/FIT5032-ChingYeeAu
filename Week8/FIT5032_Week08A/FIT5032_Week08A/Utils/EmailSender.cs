01d29f0260a95c0cb96e63acdd54f8f214fb6bc9
Ching Yee <cauu0001@student.monash.edu>
2022-09-18 00:57:00 +1000

    week 8 email

---
+++ Week8/FIT5032_Week08A/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("cyau234@gmail.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
