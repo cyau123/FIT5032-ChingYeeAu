55682710ef60916c2f8ef51058e24f2e855fe77b
chingyee <cauu0001@student.monash.edu>
2022-10-06 13:53:20 +1100

    week 10 tasks

---
+++ Week10/TodoApi/TodoApi/Models/Unit.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class Unit
+    {
+        public long Id { get; set; }
+        public string UnitName { get; set; }
+        public string UnitCode { get; set; }
+    }
+}
