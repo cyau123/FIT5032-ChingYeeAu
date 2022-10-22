c3353adb9dcb349adfdbc8a03f013159743aa809
chingyee <cauu0001@student.monash.edu>
2022-10-22 21:55:36 +1100

    update about page to include rating

--- Assignment/FIT5032_Assignment_v1/FIT5032_Assignment_v1/Controllers/HomeController.cs
+++ Assignment/FIT5032_Assignment_v1/FIT5032_Assignment_v1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FIT5032_Assignment_v1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace FIT5032_Assignment_v1.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+        private FIT5032_Models db = new FIT5032_Models();
         public ActionResult Index()
         {
             return View();
@@ -17,6 +19,9 @@ namespace FIT5032_Assignment_v1.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.Mark = db.Dentists.Find(1).AggregatedRating.ToString();
+            ViewBag.Darby = db.Dentists.Find(2).AggregatedRating.ToString();
+            ViewBag.Jacky = db.Dentists.Find(3).AggregatedRating.ToString();
 
             return View();
         }
